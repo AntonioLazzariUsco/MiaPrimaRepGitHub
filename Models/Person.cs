7c023ca9a930d27cd4649d0c4b17cc238f246567
Antonio Lazzari <antonio.lazzari@usco.it>
2023-03-31 14:42:35 +0200

    creata console

---
+++ Models/Person.cs
@@ -0,0 +1,17 @@
+namespace ConsoleApp1.Models;
+
+public class Person
+{
+ public int Id { get; set; }
+
+ public string? Name { get; set; }
+
+ public string? Description { get; set; }
+
+ public int Age { get; set; }
+
+ public int Balance { get; set; }
+
+ public int Amount { get; set; }
+
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1.Models;
+
+Console.WriteLine("Hello, World!");
+
+var person = new Person { Id = 1, Name = "Mario Rossi", Age = 35 };
+
+Console.WriteLine($"Ciao, {person.Name}");
