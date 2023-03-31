cc932874f83f4aebabd0049d9452302f1155e1df
Antonio Lazzari <antonio.lazzari@usco.it>
2023-03-31 15:02:40 +0200

    Aggiunta seconda persona

--- Program.cs
+++ Program.cs
@@ -4,5 +4,8 @@ using ConsoleApp1.Models;
 Console.WriteLine("Hello, World!");
 
 var person = new Person { Id = 1, Name = "Mario Rossi", Age = 35 };
+var person2 = new Person { Id = 1, Name = "Luigi Bianchi", Age = 45 };
 
 Console.WriteLine($"Ciao, {person.Name}");
+Console.WriteLine($"Ciao, {person2.Name}");
+
