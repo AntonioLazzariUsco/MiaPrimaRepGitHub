e0f7ff92db97fe242f91d1fb25fb89ec40db7920
Antonio Lazzari <antonio.lazzari@usco.it>
2023-03-31 15:24:01 +0200

    Aggiunto Antonio

--- Program.cs
+++ Program.cs
@@ -4,5 +4,7 @@ using ConsoleApp1.Models;
 Console.WriteLine("Hello, World!");
 var person = new Person { Id = 1, Name = "Mario Rossi", Age = 35 };
 var person2 = new Person { Id = 1, Name = "Luigi Bianchi", Age = 45 };
+var person3 = new Person { Id = 1, Name = "Antonio Lazzari", Age = 48 };
 Console.WriteLine($"Ciao, {person2.Name}");
 Console.WriteLine($"Buonasera , {person.Name}");
+Console.WriteLine($"Ciao , {person3.Name}");
